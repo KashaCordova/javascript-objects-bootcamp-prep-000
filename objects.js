e4419c0199ba06213519d42ee03d23c4bc8922f2
Celia Gearin <cgearin18@gmail.com>
2018-05-11 00:04:38 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,3 +6,6 @@ var playlist = {
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
 }
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+}
